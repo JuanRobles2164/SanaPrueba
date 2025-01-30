659437d6441d6bccc0823c4052ec8ba89b553dd8
Juan Robles <juanestebanrobles3103@gmail.com>
2025-01-30 16:18:19 -0500

    ProductControlador
    - Se agrega el ProductController y el endpoint que retornará los datos

---
+++ Backend/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Domains;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        private readonly IProductDomain _domain;
+        public ProductController(IProductDomain domain) 
+        {
+            _domain = domain;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _domain.GetProducts());
+        }
+    }
+}
